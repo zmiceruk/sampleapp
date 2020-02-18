57734f69769a36f4d7c9ae7f545fb9dbc9f6f1da
Dzmitry Babrovich <babrovich@googlemail.com>
2020-02-18 15:31:25 +0000

    dding localdatetime backport

--- app/src/main/java/com/dbabrovich/sampleapplication/application/SampleApplication.kt
+++ app/src/main/java/com/dbabrovich/sampleapplication/application/SampleApplication.kt
@@ -2,6 +2,7 @@ package com.dbabrovich.sampleapplication.application
 
 import android.app.Application
 import com.dbabrovich.sampleapplication.di.createModules
+import com.jakewharton.threetenabp.AndroidThreeTen
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
@@ -9,6 +10,9 @@ import org.koin.core.context.startKoin
 class SampleApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+
+        //Initialise ThreeTen Android Backport
+        AndroidThreeTen.init(this)
 
         //Initiate DI
         startKoin {
