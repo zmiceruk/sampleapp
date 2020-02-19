91b01fdc9af4d0465a2342eb1f67859e9d377e78
Dzmitry Babrovich <babrovich@googlemail.com>
2020-02-19 14:16:27 +0000

    Adding model class for feed.

--- domain/src/main/java/com/dbabrovich/basic/Models.kt
+++ domain/src/main/java/com/dbabrovich/basic/Models.kt
@@ -1,2 +1,35 @@
 package com.dbabrovich.basic
 
+import org.threeten.bp.LocalDateTime
+
+/**
+ * Contains information about a match
+ */
+data class Match(
+    val id: String,
+    val feedMatchId: Long,
+    val homeTeamName: String,
+    val homeTeamId: String,
+    val homeScore: Long,
+    val awayTeamName: String,
+    val awayTeamId: String,
+    val awayScore: Long,
+    val competitionId: Long,
+    val competition: String
+)
+
+/**
+ * Contains basic information about a comment
+ */
+data class Comment(
+    val type: String,
+    val comment: String,
+    val period: Long,
+    val time: String = ""
+)
+
+data class CommentaryFeed(
+    val match: Match,
+    val comments: Collection<Comment>,
+    val createdAt: LocalDateTime
+)
--- domain/src/main/java/com/dbabrovich/basic/remote/MobileRemote.kt
+++ domain/src/main/java/com/dbabrovich/basic/remote/MobileRemote.kt
@@ -1,5 +1,15 @@
 package com.dbabrovich.basic.remote
 
+import com.dbabrovich.basic.CommentaryFeed
+import io.reactivex.Single
+
+/**
+ * Interface for downloading
+ */
 interface MobileRemote {
 
+    /**
+     * Used to download commentary feed
+     */
+    fun getCommentary(): Single<CommentaryFeed>
 }
