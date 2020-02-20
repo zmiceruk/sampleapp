643447e585481885a58cd397e4f7eb441d556ce9
Dzmitry Babrovich <babrovich@googlemail.com>
2020-02-20 14:29:42 +0000

    Fixed app crashing due to lack of databinding support in the top module.

--- apppresentation/src/main/java/com/dbabrovich/apppresentation/MainActivity.kt
+++ apppresentation/src/main/java/com/dbabrovich/apppresentation/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.dbabrovich.apppresentation
 
 import android.os.Bundle
+import android.util.SparseArray
+import android.view.LayoutInflater
+import com.dbabrovich.apppresentation.databinding.ActivityMainBinding
 import com.dbabrovich.apppresentation.presenter.MainActions
 import com.dbabrovich.apppresentation.presenter.MainPresenter
 import com.dbabrovich.apppresentation.presenter.MainView
 import com.dbabrovich.apppresentation.presenter.MainViewState
-import com.dbabrovich.appusercases.interactor.CommentaryInteractor
+import com.dbabrovich.apppresentation.recyclerview.CellCommentary
+import com.dbabrovich.apppresentation.recyclerview.RecyclerDiffCallback
+import com.dbabrovich.apppresentation.recyclerview.RecyclerViewBase
+import com.dbabrovich.domain.CommentaryUseCases
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.rxkotlin.plusAssign
@@ -15,13 +21,49 @@ import net.grandcentrix.thirtyinch.TiActivity
 import org.koin.android.ext.android.get
 import java.util.concurrent.TimeUnit
 
+/**
+ * Array that contains cell view holder creators used by the recycler view
+ */
+private val viewHolderCreators =
+    SparseArray<RecyclerViewBase.ViewHolderBaseCreator<RecyclerViewBase.ViewHolderBase>>().apply {
+        put(CellCommentary.VIEW_ID, CellCommentary.Creator)
+    }
+
+/**
+ *Recycler view adapter for today's bookings
+ */
+private class RecyclerViewAdapter :
+    RecyclerViewBase.ViewAdapter<RecyclerViewBase.ViewHolderBase>(
+        viewHolderCreators, mutableListOf()
+    )
+
+private class DiffCallback(
+    private val oldList: List<RecyclerViewBase.ViewModelBase>,
+    private val newList: List<RecyclerViewBase.ViewModelBase>
+) : RecyclerDiffCallback(oldList, newList) {
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        val oldViewModel = oldList.getOrNull(oldItemPosition)
+        val newViewModel = newList.getOrNull(newItemPosition)
+        return if (oldViewModel is CellCommentary.ViewModel && newViewModel is CellCommentary.ViewModel) {
+            oldViewModel == newViewModel
+        } else false
+    }
+
+    override fun getChangePayload(oldItemPosition: Int, newItemPosition: Int): Any? {
+        return newList[newItemPosition]
+    }
+}
+
 class MainActivity : TiActivity<MainPresenter, MainView>(), MainView {
+
+    private lateinit var viewBinding: ActivityMainBinding
 
     override fun providePresenter(): MainPresenter = get()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        viewBinding = ActivityMainBinding.inflate(LayoutInflater.from(this))
+        setContentView(viewBinding.root)
     }
 
     override fun render(viewState: MainViewState) {
@@ -30,7 +72,7 @@ class MainActivity : TiActivity<MainPresenter, MainView>(), MainView {
 }
 
 class AndroidMainPresenter(
-    private val commentsInteractor: CommentaryInteractor
+    private val commentsInteractor: CommentaryUseCases
 ) : MainPresenter() {
 
     //For queueing action commands
